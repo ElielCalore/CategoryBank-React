7fbfebc5053168b7bd98f8e1cae6a8ee2c9011b7
Caio Garcia <caiopassador@hotmail.com>
2022-07-22 13:01:10 +0100

    bank models testing

--- src/pages/csvParser/index.js
+++ src/pages/csvParser/index.js
@@ -1,11 +1,15 @@
 import Papa from "papaparse";
 import { useState } from "react";
+import styles from "./styles.module.css";
+
+import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
 export function CSVParser() {
+  const navigate = useNavigate();
   const banks = {
-    nubank: {
+    Nubank: {
       delimiter: "",
       columns: [""],
       date: "",
@@ -13,39 +17,68 @@ export function CSVParser() {
       credit: "",
       debit: "",
     },
-    monzo: {
-      delimiter: "",
-      columns: [""],
-      date: "",
-      description: "",
-      credit: "",
-      debit: "",
+    Monzo: {
+      delimiter: ",",
+      columns: [
+        "Transaction ID",
+        "Date",
+        "Time",
+        "Type",
+        "Name",
+        "Emoji",
+        "Category",
+        "Amount",
+        "Currency",
+        "Local amount",
+        "Local currency",
+        "Notes and #tags",
+        "Address",
+        "Receipt",
+        "Description",
+        "Category split",
+        "Money Out",
+        "Money In",
+      ],
+      date: "Date",
+      description: "Description",
+      credit: "Money In",
+      debit: "Money Out",
     },
-    halifax: {
-      delimiter: "",
-      columns: [""],
-      date: "",
-      description: "",
-      credit: "",
-      debit: "",
+    Halifax: {
+      delimiter: ",",
+      columns: [
+        "Transaction Date",
+        "Transaction Type",
+        "Sort Code",
+        "Account Number",
+        "Transaction Description",
+        "Debit Amount",
+        "Credit Amount",
+        "Balance",
+      ],
+      date: "Transaction Date",
+      description: "Transaction Description",
+      credit: "Credit Amount",
+      debit: "Debit Amount",
     },
   };
 
+  const [bankModel, setBankModel] = useState("");
   const [transactions, setTransactions] = useState([]);
 
   let data = [];
 
-  function createObject(d) {
-    // console.log(typeof d);
+  function createObject(d, b) {
+    // console.log(b);
     d.map((currEle) => {
-      //   console.log(currEle["Transaction Date"]);
+      // console.log(currEle[banks[b]["date"]]);
       return setTransactions([
         ...transactions,
         transactions.push({
-          date: currEle["Transaction Date"],
-          description: currEle["Transaction Description"],
-          credit: currEle["Credit Amount"],
-          debit: currEle["Debit Amount"],
+          date: currEle[banks[b]["date"]],
+          description: currEle[banks[b]["description"]],
+          credit: Number(currEle[banks[b]["credit"]]),
+          debit: Number(currEle[banks[b]["debit"]]),
         }),
       ]);
     });
@@ -56,14 +89,19 @@ export function CSVParser() {
   }
 
   function processCSV(e) {
+    setTransactions([]);
+    console.log(transactions);
+    console.log(bankModel);
     const files = e.target.files;
     Papa.parse(files[0], {
       skipEmptyLines: true,
       header: true,
+      columns: banks[bankModel].columns,
+      delimiter: banks[bankModel].delimiter,
       complete: function (results) {
         data = results.data;
         console.log(data);
-        createObject(data);
+        createObject(data, bankModel);
       },
     });
 
@@ -97,7 +135,28 @@ export function CSVParser() {
 
   return (
     <>
-      <div>
+      <div className={styles.csvForm}>
+        <select
+          value={bankModel}
+          onChange={(e) => {
+            setBankModel(e.target.value);
+            // console.log(bankModel);
+          }}
+        >
+          <option value="" disabled defaultValue="Select your bank">
+            Select your bank
+          </option>
+          <option>Nubank</option>
+          <option>Halifax</option>
+          <option>Monzo</option>
+          <option
+            onClick={() => {
+              return navigate("/login");
+            }}
+          >
+            Create new model
+          </option>
+        </select>
         <input type="file" accept=".csv" onChange={processCSV} />
         <button onClick={sendToBack}>SEND</button>
 
@@ -114,3 +173,18 @@ export function CSVParser() {
     </>
   );
 }
+
+//TESTING AMOUNT AS FIELD (RATHER THAN CREDIT AND DEBIT)
+// let MoneyIn = "20";
+// let MoneyOut = "";
+// let Amount = "";
+
+// let realValue = 0;
+
+// if (Number(Amount) != 0) {
+//   realValue = Number(Amount);
+// } else {
+//   realValue = Number(MoneyIn) + Number(MoneyOut);
+// }
+
+// console.log(realValue);
