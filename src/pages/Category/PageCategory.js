9fcbb913a2c60b6c0d8bf91c6575645ae9ccfc43
Adriano Harm <adhkr81@gmail.com>
2022-07-27 18:52:41 -0300

    category html table

--- src/pages/Category/PageCategory.js
+++ src/pages/Category/PageCategory.js
@@ -5,82 +5,69 @@ import { Toaster, toast } from "react-hot-toast";
 import { LoggedNavbar } from "../../components/LoggedNavbar/index";
 
 export function PageCategory() {
-  const [data, setData] = useState([
-    { name: "", description: "", transactions: [] },
-  ]);
+	const [data, setData] = useState([{ name: "", description: "", transactions: [] }]);
 
-  useEffect(() => {
-    async function Categories() {
-      try {
-        const response = await api.get("/category/categories");
-        setData(response.data);
-      } catch (error) {
-        if (error) {
-          return toast.error("could not load categories");
-        }
-      }
-    }
-    Categories();
-  }, []);
+	useEffect(() => {
+		async function Categories() {
+			try {
+				const response = await api.get("/category/categories");
+				setData(response.data);
+			} catch (error) {
+				if (error) {
+					return toast.error("could not load categories");
+				}
+			}
+		}
+		Categories();
+	}, []);
 
-  return (
-    <div>
-      <LoggedNavbar />
-      <div className="container-fluid mt-5">
-        <Toaster />
-        <div>
-          <h2 className="text-center m-5">Categories</h2>
-        </div>
+	return (
+		<div>
+			<LoggedNavbar />
+			<Toaster />
 
-        <div className="card-body">
-            <Link to="/category/create" className="btn btn-primary">
-              <button class="btn btn-primary btn-lg px-5 mx-5" type="button">
-                Create Category
-              </button>
-            </Link>
-          </div>
+			<h2>Categories</h2>
 
-        <div className="col-3"></div>
-        <div className="col-3"></div>
+			<div>
+				<Link to="/category/create" className="btn btn-primary">
+					<button class="btn btn-primary btn-lg px-5 mx-5" type="button">
+						Create Category
+					</button>
+				</Link>
+			</div>
 
-        {data.map((current) => {
-          return (
-            <div
-              className="container mb-1 border border-dark"
-              key={current.code}
-            >
-              <div className="row mb-3 p-4 align-items-center">
-                <div className="col-3">
-                  <strong>CODE: </strong>
-                  <p>{current.code}</p>
-                </div>
-                <div className="col-7">
-                  <strong>DESCRIPTION: </strong>
-                  <p>{current.description}</p>
-                </div>
-                <div class="col gap-0  mx-auto py-3 m-3">
-                  <Link to={`/category/edit/${current._id}`}>
-                    <button
-                      class="btn btn-primary btn-lg px-3 mx-1"
-                      type="button"
-                    >
-                      EDIT
-                    </button>
-                  </Link>
-                  <Link to={`/category/delete/${current._id}`}>
-                    <button class="btn btn-primary btn-lg px-2" type="button">
-                      DELETE
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-        <div className="d-flex p-3 flex-column mb-10 text-center">
-
-        </div>
-      </div>
-    </div>
-  );
+			<table className="table">
+				<thead>
+					<th>Code</th>
+					<th>Description</th>
+					<th></th>
+					<th></th> 
+				</thead>
+        <tbody>
+				{data.map((current) => {
+					return (
+						<tr>
+							<td>
+								<p>{current.code}</p>
+							</td>
+							<td>
+								<p>{current.description}</p>
+							</td>
+							<td>
+								<Link to={`/category/edit/${current._id}`}>
+									<button class="btn btn-primary btn-lg px-3 mx-1">EDIT</button>
+								</Link>
+							</td>
+							<td>
+								<Link to={`/category/delete/${current._id}`}>
+									<button class="btn btn-danger btn-lg px-2">DELETE</button>
+								</Link>
+							</td>
+						</tr>
+					);
+				})}
+        </tbody>
+			</table>
+		</div>
+	);
 }
